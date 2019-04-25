9320e7ad1d75ceb4ce56c4e6e90dbc843d4af44d
yuanshaoyi <740161499@qq.com>
2019-04-25 20:48:51 +0800

    b

---
+++ code/minist/alexnet.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(AlexNet, self).__init__()
+
+        self.module_name = 'alexnet'
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 11, 4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, 5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(192, 384, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        
+        x = self.classifier(x.view(x.size(0), -1))
+        return x
